121b232b9724fdfa98695701569929f5966960de
VdoVd <liuhaobin486@gmail.com>
2024-05-13 15:31:43 +0800

    index add

--- server/shengling/src/main/java/com/example/crm/controller/LoginController.java
+++ server/shengling/src/main/java/com/example/crm/controller/LoginController.java
@@ -3,6 +3,7 @@ import com.example.crm.Mapper.UserMapper;
 import com.example.crm.pojo.User;
 import com.example.crm.pojo.req.Login;
 import com.example.crm.result.AllReturn;
+import com.example.crm.util.jwtTool;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +21,8 @@ public class LoginController {
     @Autowired
     AllReturn allReturn;
 
+
+
     @PostMapping("/login")
     public String login(@RequestBody Login login){
 
@@ -28,11 +31,18 @@ public class LoginController {
         map.put("name",login.getName());
         map.put("password",login.getPass());
         List<User> list=userMapper.selectByMap(map);
-
-        allReturn.setCode(200);
-        allReturn.setMessage("登录成功");
-        allReturn.setSuccess(true);
-        allReturn.setData(new Gson().toJson(list.get(0)));
+        String token=jwtTool.getToken(login.getName(),login.getPass());
+        if(list.size()!=0){
+            allReturn.setCode(200);
+            allReturn.setMessage("登录成功");
+            allReturn.setSuccess(true);
+            allReturn.setData(token);
+        }else {
+            allReturn.setCode(400);
+            allReturn.setMessage("登录失败");
+            allReturn.setSuccess(true);
+            allReturn.setData("");
+        }
 
         System.out.println(new Gson().toJson(allReturn));
 
